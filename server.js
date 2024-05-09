b7264a2bca19227f5d3202401bf6b32e7cfd6cf8
pedroquintao <pedro.h.quintao@hotmail.com>
2024-05-08 23:44:55 -0300

    Finalizada a Aula 02 Atividade 6: Criando registros com POST

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-import http from "http";
+// import http from "http";
+import app from './src/app.js';
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain"})
-    res.end('Teste')
-})
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log('Servidor escutando!');
 });

a2040bb8d31c8400bb7b82fbd03251e1134c1452
pedroquintao <pedro.h.quintao@hotmail.com>
2024-05-10 23:01:21 -0300

    Aula 04 finalizada. Nesse ponto já é possível utilizar as informações para concluir o projeto cadus. Foi ensinado como organizar e criar as rotas, controladores, modelos e requisições. Além disso foi mostrado como criar um banco de dados na nuvem, conectar-se à ele e manipulá-lo

--- src/app.js
+++ src/app.js
@@ -1,46 +1,21 @@
-import express from 'express'
+import express from 'express';
+import conectaNaDatabase from './config/dbConect.js';
+import livro from './models/Livro.js';
+import routes from "./routes/index.js"
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+    console.error("Erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+    console.log("Conexão com o banco realizada com sucesso!")
+})
 
 const app = express();
-app.use(express.json());
 
-const livros = [
-    {
-        id: 1,
-        titulo: 'O senhor dos Anéis'
-    },
-    {
-        id: 2,
-        titulo: 'O Hobbit'
-    }
-];
-
-function buscaLivro(id) {
-    return livros.findIndex(livro => { return livro.id === Number(id) })
-}
-
-app.get('/', (req, res) => {
-    res.status(200).send('Curso de Node.js');
-});
-
-app.get('/livros', (req, res) => {
-    res.status(200).json(livros);
-});
-
-app.get('/livros/:id', (req, res) => {
-    const index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index])
-})
-
-app.post('/livros', (req, res) => {
-    livros.push(req.body);
-    res.status(201).send('Livro cadastrado com sucesso');
-});
-
-app.put('/livros/:id', (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.status(200).json(livros)
-})
+routes(app);
 
 app.delete('/livros/:id', (req, res) => {
     const index = buscaLivro(req.params.id)
@@ -49,3 +24,4 @@ app.delete('/livros/:id', (req, res) => {
 })
 
 export default app;
+
---
+++ src/routes/livrosRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import LivroController from "../controller/livroController.js";
+
+const routes = express.Router();
+
+routes.get("/livros", LivroController.listarLivros);
+routes.get("/livros/:id", LivroController.listarLivroPorId);
+routes.post("/livros", LivroController.cadastrarLivro);
+routes.put("/livros/:id", LivroController.atualizarLivro);
+routes.delete("/livros/:id", LivroController.excluirLivro);
+
+
+
+
+
+export default routes;
